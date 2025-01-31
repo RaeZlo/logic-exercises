82b3057533b81189e453161118a4f18427346c4e
RaeZlo <rramirezperazzolo@gmail.com>
2025-01-31 16:06:38 -0300

    Update remover_caracteres.py

--- codewars/remover_caracteres.py
+++ codewars/remover_caracteres.py
@@ -3,7 +3,7 @@ Crea una función que elimina el primer y último carácter de una cadena.
 """
 
 def remover_caracteres(palabra:str) -> str:
-    # La sintaxis s[1:-1] realiza un "slice" o rebanada de la cadena.
+    # La sintaxis palabra[1:-1] realiza un "slice" o rebanada de la cadena.
     # 1: Indica el inicio del corte (el segundo carácter, ya que los índices empiezan en 0).
     # -1: Indica el fin del corte (el penúltimo carácter, ya que los índices negativos cuentan desde el final).
     # Al omitir el primer y último índice, se eliminan el primer y último carácter de la cadena.
