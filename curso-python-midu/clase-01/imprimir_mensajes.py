febd43116cecd33a957249c733a0fc9710f39d25
RaeZlo <rramirezperazzolo@gmail.com>
2025-02-12 15:53:19 -0300

    Create imprimir_mensajes.py

---
+++ curso-python-midu/clase-01/imprimir_mensajes.py
@@ -0,0 +1,5 @@
+"""
+Ejercicio 1: Imprime un mensaje
+Escribe un programa que imprima tu nombre y tu ciudad en líneas separadas.
+"""
+print("¡Hola, mundo! Este es el primer ejercicio de programación.")
