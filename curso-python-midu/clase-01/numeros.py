283cd1f9f064a62b3686c5d02d49fd32e6ce2183
RaeZlo <rramirezperazzolo@gmail.com>
2025-02-12 16:10:37 -0300

    Create numeros.py

---
+++ curso-python-midu/clase-01/numeros.py
@@ -0,0 +1,12 @@
+"""
+Ejercicio 5: Números
+1. Crea una variable con el número PI (sin asignar una variable)
+2. Redondea el número con round()
+3. Haz la división entera entre el número que te salió y el número 2
+4. El resultado debería ser 1
+"""
+
+print("El valor aproximado de PI es: ", 3.1416)
+print("El valor de PI redondeado es: ", round(3.1416))
+resultado = round(3.1416) // 2
+print("División entera de PI redondeado entre 2:", resultado)
