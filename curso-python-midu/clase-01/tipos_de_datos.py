08db02a839dddba09e2123a124858153cd52ffd2
RaeZlo <rramirezperazzolo@gmail.com>
2025-02-12 15:56:55 -0300

    Create tipos_de_datos.py

---
+++ curso-python-midu/clase-01/tipos_de_datos.py
@@ -0,0 +1,15 @@
+"""
+Ejercicio 2: Muestra los tipos de datos de las siguientes variables.
+Usa el comando 'type()' para determinar el tipo de datos de cada variable.
+"""
+a = 15
+b = 3.14159
+c = "Hola mundo"
+d = True
+e = None
+
+print(f"El tipo de a es: {type(a)}")
+print(f"El tipo de b es: {type(b)}")
+print(f"El tipo de c es: {type(c)}")
+print(f"El tipo de d es: {type(d)}")
+print(f"El tipo de e es: {type(e)}")
