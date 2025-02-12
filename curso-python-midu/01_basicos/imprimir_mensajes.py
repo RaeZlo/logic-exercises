b7d6a790ee8267fe291ea8b2691751698efafa9f
RaeZlo <rramirezperazzolo@gmail.com>
2025-02-12 16:17:36 -0300

    Update and rename imprimir_mensajes.py to imprimir_mensajes.py

--- curso-python-midu/01_basicos/imprimir_mensajes.py
+++ curso-python-midu/01_basicos/imprimir_mensajes.py
@@ -3,3 +3,4 @@ Ejercicio 1: Imprime un mensaje
 Escribe un programa que imprima tu nombre y tu ciudad en líneas separadas.
 """
 print("¡Hola, mundo! Este es el primer ejercicio de programación.")
+print(1900 + 85)
