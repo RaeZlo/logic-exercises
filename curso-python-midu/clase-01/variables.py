79f7a156a974f4f43f65156e4c281fc799b31394
RaeZlo <rramirezperazzolo@gmail.com>
2025-02-12 16:02:13 -0300

    Create variables.py

---
+++ curso-python-midu/clase-01/variables.py
@@ -0,0 +1,10 @@
+"""
+Ejercicio 4: Variables
+Crea variables para tu nombre, edad y altura.
+Usa f-strings para imprimir una presentación.
+"""
+
+nombre, edad, altura = "Rafael", 20, 175
+
+# Usamos f-string para imprimir los valores de las variables de manera clara.
+print(f"Me llamo {nombre}, tengo {edad} años y mido {altura} cm.")
