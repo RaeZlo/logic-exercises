0e5c8f1e4f63c220e99cdd34c382bb7b633f1f4c
RaeZlo <rramirezperazzolo@gmail.com>
2025-02-15 15:16:27 -0300

    Create duplicar_lista.py

---
+++ curso-python-midu/02_control_de_flujo/if/duplicar_lista.py
@@ -0,0 +1,12 @@
+"""
+Ejercicio 4: Duplicando la lista
+Dada una lista:
+lista = [1, 2, 3]
+Crea una nueva lista que contenga los elementos de la lista original duplicados.
+Ejemplo: [1, 2, 3] -> [1, 2, 3, 1, 2, 3]
+"""
+
+lista = [1, 2, 3]
+nueva_lista = lista * 2 # Tambien se puede expresar como: lista + lista
+
+print(nueva_lista)
