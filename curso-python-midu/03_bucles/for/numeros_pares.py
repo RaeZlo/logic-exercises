e0198c4698714932805de0825a719ae7dba0d0e5
RaeZlo <rramirezperazzolo@gmail.com>
2025-02-18 15:51:51 -0300

    Create numeros_pares.py

---
+++ curso-python-midu/03_bucles/for/numeros_pares.py
@@ -0,0 +1,9 @@
+"""
+Ejercicio 1: Imprimir números pares
+Imprime todos los números pares del 2 al 20 (inclusive) usando un bucle for.
+"""
+
+for num in range(2, 21):
+    if num % 2 == 0:
+        print(num)
+      
