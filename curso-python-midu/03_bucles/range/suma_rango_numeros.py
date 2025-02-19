d0b8975f982cbc16bb7836ff316990968ce0181d
RaeZlo <rramirezperazzolo@gmail.com>
2025-02-19 14:27:36 -0300

    Create suma_rango_numeros.py

---
+++ curso-python-midu/03_bucles/range/suma_rango_numeros.py
@@ -0,0 +1,10 @@
+"""
+Ejercicio 5: Suma de números en un rango
+Calcula la suma de los números del 1 al 100 (inclusive) usando un bucle for y range().
+"""
+
+suma = 0
+for num in range(1, 101):
+    suma += num
+
+print(suma)
