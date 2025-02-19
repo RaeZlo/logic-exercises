9cd87d55636fc9cf1fdfed32cc932a5248ae4014
RaeZlo <rramirezperazzolo@gmail.com>
2025-02-19 14:19:56 -0300

    Create orden_inverso.py

---
+++ curso-python-midu/03_bucles/range/orden_inverso.py
@@ -0,0 +1,8 @@
+"""
+Ejercicio 4: Imprimir números en orden inverso
+Imprime los números del 10 al 1 (inclusive) en orden inverso usando un bucle for y range().
+"""
+
+for num in range(10, 0, -1):
+    print(num)
+  
